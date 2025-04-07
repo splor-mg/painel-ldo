406ab7c52ff32c136db73c6cb159632559b4cf90
labanca <moraislabanca@gmail.com>
2025-04-07 13:08:23 -0300

    fix(fonte-analise): corrige posicao relativa das colunas nos alertas da base fonte-analise. see #13.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from databases import carrega_trata_dados, cria_base_fonte_analise, cria_base_receita_analise
 
-   
+
 valor_painel = carrega_trata_dados()
 
 cria_base_receita_analise(valor_painel=valor_painel)
